675f9da166c4a170cd35d8621e5c9c752a35ee83
MauGill <84589470+MauGill@users.noreply.github.com>
2021-08-23 12:52:36 -0300

    Add files via upload
    Aplico efecto typing

--- js/js.js
+++ js/js.js
@@ -20,57 +20,22 @@ const navSlide = () => {
 }
 navSlide();
 
-
-// class MobileNavbar {
-//     constructor(mobileMenu, navList, navLinks) {
-//         this.mobileMenu = document.querySelector (mobileMenu);
-//         this.navList = document.querySelector (navList);
-//         this.navLinks = document.querySelector (navLinks);
-//         this.activeClass ="active";
-//         this.handleClick = this.handleClick.bind(this);
-//     }
-
-//     animateLinks() {
-//         this.navLinks.forEach((link) => {
-//             link.style.animation
-//                 ? (link.style.animation = "")
-//                 : (link.style.animation = `navLinkFade 0.5s ease forwards 0.3s`);
-//         });
-//     }
-
-//     handleClick(){
-//         console.log(this)
-//         this.navList.classList.toggle(this.activeClass);
-//         this.animateLinks();
-//     }
-
-
-//     addClickEvent () {
-//         this.mobileMenu.addEventListener ("click", this.handleClick );
-//     }
-//     init () {
-//         if (this.mobileMenu) {
-//             this.addClickEvent();
-//         }
-//         return this;
-//     }
-// }
-// const mobileNavbar = new MobileNavbar(
-//     ".mobile-menu",
-//     ".nav-list",
-//     ".nav-list li",
-// ); 
-// mobileNavbar.init();
-
-
-
-
-
-// let nav = document.querySelector('body .header .nav-var .mobile-menu');
-// let links = document.querySelector('body .header .nav-var .links ul')
-
-// nav.addEventListener('click', desplazar);
-
-// function desplazar () {
-//     links.classList.toggle("active");
-// }
+const typed = new Typed ('.typed', {
+    strings: [
+    ' <i class="weare" > projects </i> ',
+    ' <i class="weare" > buildings </i> ',
+    ' <i class="weare" > surveys </i> ',
+    ' <i class="weare" > remodeling </i> '],
+    //stringsElement: '#cadenas-texto',
+    typeSpeed: 75,
+    startDelay: 300,
+    backSpeed: 75,
+    smartBackspace: true,
+    shuffle: false ,
+    backDelay: 1500,
+    loop: true,
+    loopCount: false,
+    showCursor: true,
+    cursorChar: '|',
+    contentType: 'html',
+});
