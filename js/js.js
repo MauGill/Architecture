812adc7c738408073c04b65e688d8e7adf172727
MauGill <84589470+MauGill@users.noreply.github.com>
2021-09-13 20:11:23 -0300

    Add files via upload
    Add gallery

--- js/js.js
+++ js/js.js
@@ -115,3 +115,28 @@ function shownav () {
         navVar.classList.toggle("sticky", window.scrollY > 350)
 
 }
+//                                                                                                                          GALLERY
+const modal = document.querySelector(".modal");
+const previews = document.querySelectorAll(".gallery img")
+const original = document.querySelector(".full-img")
+const caption = document.querySelector(".caption");
+
+previews.forEach(preview =>  {
+    preview.addEventListener('click', () => {
+        modal.classList.add("open");
+        original.classList.add("open");
+        const originalSrc = preview.getAttribute("data-original");
+        original.src= `./img/${originalSrc}`;
+        const altText = preview.alt
+        caption.textContent = altText;
+    } )
+});
+modal.addEventListener("click", (e) => {
+    if (e.target.classList.contains("modal")) {
+        modal.classList.remove("open");
+        original.classList.remove("open");
+    }
+} )
+
+
+
